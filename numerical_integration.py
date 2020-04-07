193b4cc260c9785fd93f14ef2b9b4306c517bc21
Diego Holanda <diegoholanda2008@hotmail.com>
2020-04-07 17:16:35 -0300

    Add function to generate the results of the integration

--- numerical_integration.py
+++ numerical_integration.py
@@ -1,5 +1,5 @@
 import math
-
+from algorithms import *
 
 def function(x):
     '''Function f(x) to be integrated'''
@@ -7,95 +7,15 @@ def function(x):
 
 integral_precisa = 17.8764703
 
-# ======================================= #
-#       Fórmulas de newton-colos          #
-# ======================================= #
-
-def regra_trapezio(f, xi, xf, k, delta_x):
-    return (delta_x/2)*(f(xi) + f(xf)) 
-
-def regra_trapezio_aberto(f, xi, xf, k, delta_x):
-    h = delta_x / 3
-    return (delta_x/2) * (f(xi + h) + f(xi + 2*h))
-
-def regra_simpson(f, xi, xf, k, delta_x):
-    h = delta_x / 2
-    return (h/3)*(f(xi) + 4*f(xi+h) + f(xf))
-
-def regra_milne(f, xi, xf, k, delta_x): 
-    h = delta_x / 4
-    return (delta_x/3) * (2*f(xi + h) - f(xi + 2*h) + 2*f(xi + 3*h))
-
-def regra_simpson_3_8(f, xi, xf, k, delta_x):
-    h = delta_x / 3
-    return (3*h/8) * (f(xi) + 3*f(xi + h) + 3*f(xi + 2*h) + f(xf))
-
-def regra_poli_3_aberta(f, xi, xf, k, delta_x):
-    h = delta_x / 5
-    return (5*h/24) * (11*f(xi + h) + f(xi + 2*h) + f(xi + 3*h) + 11*f(xi + 4*h))
-
-def regra_poli_4_fechada(f, xi, xf, k, delta_x):
-    h = delta_x / 4
-    return (2*h/45) * (7*f(xi) + 32*f(xi + h) + 12*f(xi + 2*h) + 32*f(xi + 3*h) + 7*f(xf))
-
-def regra_poli_4_aberta(f, xi, xf, k, delta_x):
-    h = delta_x / 6
-    return (3*h/10) * (11*f(xi + h) - 14*f(xi + 2*h) + 26*f(xi + 3*h) - 14*f(xi + 4*h) + 11*f(xi + 5*h))
-
-# ======================================= #
-#     Funções de integração numerica      # 
-# ======================================= #
-
-def integrate_n(f, a, b, n, subs_func = regra_trapezio):
-    I = 0
-    h = (b-a)/n
-
-    for k in range(n):
-        x0 = a + k*h
-        x1 = x0 + h
-        I += subs_func(f, x0, x1, k, h)
-    
-    return I
-
-def integrate(f, a, b, E, subs_func=regra_trapezio, debug=False):
-    I = 0
-    N = 1
-    erro = 1
-    i = 0
-    
-    if(debug): file = open(f"./out/{subs_func.__name__}.txt", "w")
-
-    while erro > E:
-        N *= 2
-        In = integrate_n(f, a, b, N, subs_func)
-        erro = abs((In - I)/In)
-        if(debug):
-            i += 1
-            file.write(f"Iteração {i}\n")
-            file.write(f"I{i-1} \t= {I:.7f}\n")
-            file.write(f"I{i} \t= {In:.7f}\n")
-            file.write(f"Erro \t= {erro}\n")
-            file.write("----------------\n")
-        I = In
-    if(debug):
-        file.write(f"Regra: {subs_func.__name__}\n")
-        file.write(f"Num. Iterações: {i}\n")
-        file.write(f"Resultado: {I:.7f}\n")
-        file.close()
-    return I
-
-# ======================================= #
-#       Testes e Execução do código       # 
-# ======================================= #
-def main():
-    trapezio = integrate(function, 0, 1, math.pow(10, -6), regra_trapezio, True)
-    trapezio_aberto = integrate(function, 0, 1, math.pow(10, -6), regra_trapezio_aberto, True)
-    simpson = integrate(function, 0, 1, math.pow(10, -6), regra_simpson, True)
-    milne = integrate(function, 0, 1, math.pow(10, -6), regra_milne, True)
-    simpson_3_8 = integrate(function, 0, 1, math.pow(10, -6), regra_simpson_3_8, True)
-    poli_3_aberta = integrate(function, 0, 1, math.pow(10, -6), regra_poli_3_aberta, True)
-    poli_4_fechada = integrate(function, 0, 1, math.pow(10, -6), regra_poli_4_fechada, True)
-    poli_4_aberta = integrate(function, 0, 1, math.pow(10, -6), regra_poli_4_aberta, True)
+def output_newton_cotes(output_to_file=True):
+    trapezio = integrate(function, 0, 1, math.pow(10, -6), regra_trapezio, output_to_file)
+    trapezio_aberto = integrate(function, 0, 1, math.pow(10, -6), regra_trapezio_aberto, output_to_file)
+    simpson = integrate(function, 0, 1, math.pow(10, -6), regra_simpson, output_to_file)
+    milne = integrate(function, 0, 1, math.pow(10, -6), regra_milne, output_to_file)
+    simpson_3_8 = integrate(function, 0, 1, math.pow(10, -6), regra_simpson_3_8, output_to_file)
+    poli_3_aberta = integrate(function, 0, 1, math.pow(10, -6), regra_poli_3_aberta, output_to_file)
+    poli_4_fechada = integrate(function, 0, 1, math.pow(10, -6), regra_poli_4_fechada, output_to_file)
+    poli_4_aberta = integrate(function, 0, 1, math.pow(10, -6), regra_poli_4_aberta, output_to_file)
 
     print(f"Trapezio Fechado \t= {trapezio:.7f}")
     print(f"Trapezio Aberto \t= {trapezio_aberto:.7f}")
@@ -106,6 +26,13 @@ def main():
     print(f"Polinomio 4 Fechada \t= {poli_4_fechada:.7f}")
     print(f"Polinomio 4 Aberta \t= {poli_4_aberta:.7f}")
     print(f"Integral \t\t= {integral_precisa}")
+
+def output_gauss_legendre(output_to_file=True):
+    gl_2p = integrate(function, 0, 1, math.pow(10, -6), gauss_legendre_2p, output_to_file)
+
+def main():
+    # output_newton_cotes()
+    output_gauss_legendre()
 
 if __name__ == "__main__":
     main()
